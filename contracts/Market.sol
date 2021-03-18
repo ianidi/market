04b4cbc994a17b64b792c63081e8e2b6b218877d
Аниди Иван <l.anidi@protonmail.com>
2021-03-19 00:39:43 +0300

    Commit

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.6.0;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
