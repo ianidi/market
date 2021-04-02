fde0a5f30c6c4e836a0dcc7097ec96cab18d119e
Аниди Иван <l.anidi@protonmail.com>
2021-04-02 11:23:57 +0300

    Commit

--- contracts/ConditionalToken.sol
+++ contracts/ConditionalToken.sol
@@ -3,11 +3,13 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-// import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ConditionalToken is ERC20, Ownable {
-    constructor(string memory _name, string memory _symbol, uint8 _decimals) ERC20(_name, _symbol, _decimals) public {}
+contract ConditionalToken is ERC20, ERC20Burnable, Ownable {
+    constructor(string memory name_, string memory symbol_, uint8 _decimals) ERC20(name_, symbol_) public {
+        _setupDecimals(decimals_);
+    }
 
     function mint(address account, uint256 amount)
         public
